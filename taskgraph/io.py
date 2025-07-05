f45429fb986aea42d32f366d14174d52274f4d84
al-jpacho <jordanapacho@gmail.com>
2025-07-05 13:38:55 +0100

    feat(io): implement logic for load_tasks with filtering, fallback, and safe parsing

--- taskgraph/io.py
+++ taskgraph/io.py
@@ -19,6 +19,31 @@ def save_tasks(json_data: list[dict], file_path: str) -> None:
         json.dump(json_data, f, indent=2)
 
 
-def load_tasks():
-    """ """
-    pass
+def load_tasks(file_path: str) -> list[dict]:
+    """
+    Load the most recent TaskGraph JSON file from a folder.
+
+    Args:
+        file_path (str): Directory path to JSON file.
+
+    Raises:
+        FileNotFoundError: No TaskGraph file found.
+
+    Returns:
+        list[dict]: List of task objects.
+    """
+    files = sorted(
+        f for f in os.listdir(file_path)
+        if f.startswith("taskgraph-") and f.endswith(".json")
+    )
+
+    if not files:
+        raise FileNotFoundError("No TaskGraph JSON files found.")
+
+    file_to_open = os.path.join(file_path, files[-1])
+
+    with open(file_to_open, "r", encoding="utf-8") as f:
+        tasks = json.load(f)
+
+    return tasks
+
