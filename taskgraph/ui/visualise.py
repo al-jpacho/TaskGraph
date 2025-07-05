c644192271d2c89bfaa16380d544ef19161e9942
al-jpacho <jordanapacho@gmail.com>
2025-07-05 16:47:15 +0100

    feat(visualise): add node colours to chart generation

--- taskgraph/ui/visualise.py
+++ taskgraph/ui/visualise.py
@@ -18,8 +18,14 @@ def generate_task_graph(tasks: list[dict]):
     """
     G = nx.DiGraph()
 
+    node_colours = []
+
     for task in tasks:
-        G.add_node(task["task"])
+        task_name = task["task"]
+        status = task["status"]
+
+        G.add_node(task_name)
+        node_colours.append(STATUS_COLOURS[status])
     
     for task in tasks:
         for dep in task["depends_on"]:
@@ -28,5 +34,5 @@ def generate_task_graph(tasks: list[dict]):
     pos = nx.spring_layout(G, k=0.5)
 
     fig, ax = plt.subplots(figsize=(8,3))
-    nx.draw(G, pos, with_labels=True, arrows=True, node_size=400, node_color='green', font_size=5, ax=ax)
+    nx.draw(G, pos, with_labels=True, arrows=True, node_size=400, node_color=node_colours, font_size=5, ax=ax)
     st.pyplot(fig) 
